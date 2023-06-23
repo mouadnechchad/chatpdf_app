931721fe9d77f9902b0d674deedf7b7583200cac
medmac01 <[th3justhacker@gmail.com]>
2023-06-23 01:59:30 +0100

    Minor changements

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from langchain_conv import *
+from embed import *
 
 app = Flask(__name__)
 
@@ -27,17 +28,16 @@ def chat():
         data = request.get_json()
         message = data['msg']
 
-        # print(data['msg'])
 
-        passage = '''
-        son peuple et le ralliement de celui-ci aux rois de l’époque ont permis de créer une vraie  nation unie.   Le pays entre dans l’ère moderne avec la dynastie alaouite. La nation marocaine s’est  dotée de fondements étatiques dont le pouvoir central était entre les mains du Makhzen 13,  d’une diplomatie incarnant l’Ét at et d’une armée nationale.    La prise d’Alger le 5 juillet 1830 a entraîné  le début d’une période tumultueuse au  Maroc. La victoire des Français, ressentie comme  une menace à l’intégrité du pays et à la foi  musulmane au Maghreb, marque le début des hos tilités franco-marocaines. En effet, Moulay  Abderrahmane (1822-1859), le sultan alaouite au pouvoir, vole au secours des habitants de  Tlemcen (Algérie) leur assurant vivres et armes. Les Français répliquent violemment et occupent non seulement la ville de Tlemcen en  1836, mais installent un fort militaire à Lalla  Maghnia, non loin de la ville marocaine d’Oujda.
-        '''
+        passage, filename, pg_number = get_chunk(message)
         # Perform chatbot processing here
         reply = conv(message,passage)
 
         response = {'question': message,
                     'passage' : passage,
-                    'answer': reply}
+                    'answer': reply,
+                    'filename': filename,
+                    'pg_number': pg_number}
         
 
         return jsonify(response)  # Return response as JSON
