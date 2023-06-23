54263f7324d72ce5d1c6da9dd41c6c338f65c4e6
medmac01 <[th3justhacker@gmail.com]>
2023-06-23 19:14:36 +0100

    Implemented cohere api + Bugfixes

--- embed.py
+++ embed.py
@@ -3,13 +3,16 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from huggingface_hub import hf_hub_download
 import pinecone
 from langchain.vectorstores import Pinecone
+import requests
+import cohere
+
 
 path="/Users/medmachrouh/Desktop/PFA/index_2.json"
 with open(path) as json_file:
     data1 = json.load(json_file)
 json_file.close()
 
-def get_chunk(query):
+def get_chunk(text):
     index_name = 'pfa'
 
     pinecone.init(
@@ -17,7 +20,32 @@ def get_chunk(query):
             environment="asia-southeast1-gcp-free"  # find next to api key in console
     )
 
-    hf_embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/gtr-t5-large')
+    index = pinecone.Index(index_name)
+
+    co = cohere.Client('xwQEiqU1kYFXZxECK7aquQPyXDx9uUTU4j44pHB2') # This is your trial API key
+    response = co.embed(
+    model='embed-multilingual-v2.0',
+    texts=[text])
+    # hf_embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/gtr-t5-large')
+
+    # API_URL = "https://api-inference.huggingface.co/models/sentence-transformers/gtr-t5-large"
+    # headers = {"Authorization": "Bearer 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"}
+
+    # def query(payload):
+    #     response = requests.post(API_URL, headers=headers, json=payload)
+    #     return response.json()
+        
+    # output = query({
+    #     "inputs": {
+    #         "source_sentence": text,
+    #         "sentences":[text]
+    #     },
+    # })
+
+
+    print('___________')
+
+
 
     batch_limit = 100
     metadatas = []
@@ -38,12 +66,8 @@ def get_chunk(query):
 
     # vectorstore = Pinecone(index,hf_embeddings.embed_query, [t['content'] for t in metadatas])
 
-    docsearch = Pinecone.from_existing_index(index_name=index_name, embedding=embeddings)
+    # docsearch = Pinecone.from_existing_index(index_name=index_name, embedding=output)
 
-    docs = docsearch.similarity_search(query,k=1)
+    docs = index.query(response.embeddings, top_k=1, include_metadata=True)
 
-    for t in metadatas:
-        if docs[0].page_content==t['content']:
-            print(docs[0].page_content,t['source'],t['page_number'])
-
-    return docs[0].page_content,t['source'],t['page_number']
+    return docs['matches'][0]['metadata']['content'],docs['matches'][0]['metadata']['source'],int(docs['matches'][0]['metadata']['page_number'])
